84cd1e0ccbec263159c2ed2e3eea012eaefdb349
aungsiphyo <aungsiphyoit@gmail.com>
2024-10-20 10:23:16 +0630

    design demo

---
+++ main.js
@@ -0,0 +1,8 @@
+var email = "someone@gmail.com";
+var password = "password";
+
+function checkUser(mail,pass){
+    if(mail == email && pass == password){
+        document.write("Login Process Successful");
+    }
+}
